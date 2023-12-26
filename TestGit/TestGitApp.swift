b9be7497cdc72b599bd35b9490fab39c8313c37a
Aleksei Tinkov <boofle@outlook.com>
2023-12-26 16:53:10 +0300

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Алексей Тиньков on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
